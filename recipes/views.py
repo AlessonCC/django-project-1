b141ec2d605093d5e2e9505be6176e67627ba848
Álesson Carlos Costa dos Santos <alessoncarlos20@gmail.com>
2022-11-12 18:26:19 -0300

    Adicinando nova URL para view

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Álesson Carlos',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Álesson Carlos',
+    })

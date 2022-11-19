f038801b3c083011bc7ad4451318a8c2eaa90875
Álesson Carlos Costa dos Santos <alessoncarlos20@gmail.com>
2022-11-19 00:10:43 -0300

    Correção de paginas que recebem html

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,6 @@
-
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 from recipes.models import Recipe
-from utils.recipes.factory import make_recipe
 
 
 def home(request):
@@ -15,23 +12,26 @@ def home(request):
     })
 
 
-def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
-        'is_datail_page': True,
+def category(request, category_id):
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Categoria | '
     })
 
 
-def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True
-    ).order_by('-id')
+def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
 
-    if not recipes:
-        raise Http404('Not Found :(')
-
-    return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Categoria | '
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': recipe,
+        'is_detail_page': True,
     })
